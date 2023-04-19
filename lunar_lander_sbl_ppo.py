6eab8a89f14a4eebd271a6ff075ad6937b191344
Dante-Basile <dbasile1070@gmail.com>
2023-04-19 12:16:56 -0400

    wind env and gif

---
+++ lunar_lander_sbl_ppo.py
@@ -0,0 +1,45 @@
+import gym
+
+from stable_baselines3 import PPO
+from stable_baselines3.common.evaluation import evaluate_policy
+
+import numpy as np
+import imageio
+
+
+train = True
+
+
+# Create environment
+env = gym.make("LunarLander-v2", enable_wind=True)
+
+if train:
+    # Instantiate the agent
+    model = PPO("MlpPolicy", env, verbose=1)
+    # Train the agent and display a progress bar
+    model.learn(total_timesteps=int(2e5), progress_bar=True)
+    # Save the agent
+    model.save("ppo_lunar_wind")
+    del model  # delete trained model to demonstrate loading
+
+# Load the trained agent
+model = PPO.load("ppo_lunar_wind", env=env)
+
+# Evaluate the agent
+mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
+
+print(f"mean reward: {mean_reward}")
+print(f"std dev reward: {std_reward}")
+
+# Enjoy trained agent
+env_disp = gym.make("LunarLander-v2", enable_wind=True, render_mode='rgb_array')
+del model
+model = PPO.load("ppo_lunar_wind", env=env_disp)
+vec_env = model.get_env()
+obs = vec_env.reset()
+frames = []
+for i in range(1000):
+    action, _states = model.predict(obs, deterministic=True)
+    obs, rewards, dones, info = vec_env.step(action)
+    frames.append(vec_env.render())
+imageio.mimsave("ppo_lunar_wind.gif", [np.array(img) for i, img in enumerate(frames) if i%2 == 0], fps=29)
--- lunar_lander_sbl_ppo_primacy.py
+++ lunar_lander_sbl_ppo_primacy.py
@@ -16,33 +16,55 @@ if train:
     # Instantiate the agent
     model_calm_wind = PPO("MlpPolicy", env_calm, verbose=1)
     # Train the agent and display a progress bar
+    print("Training calm_wind on calm")
     model_calm_wind.learn(total_timesteps=int(2e5), progress_bar=True)
     model_calm_wind.set_env(env_wind)
+    print("Training calm_wind on wind")
     model_calm_wind.learn(total_timesteps=int(2e5), progress_bar=True)
 
     model_wind_calm = PPO("MlpPolicy", env_wind, verbose=1)
+    print("Training wind_calm on wind")
     model_wind_calm.learn(total_timesteps=int(2e5), progress_bar=True)
     model_wind_calm.set_env(env_calm)
+    print("Training wind_calm on calm")
     model_wind_calm.learn(total_timesteps=int(2e5), progress_bar=True)
 
     # Save the agent
     model_calm_wind.save("ppo_lunar_primacy_calm_wind")
+    model_wind_calm.save("ppo_lunar_primacy_wind_calm")
     del model_calm_wind  # delete trained model to demonstrate loading
     del model_wind_calm
 
 # Load the trained agent
-model = PPO.load("ppo_lunar_primacy", env=env)
+model_calm_wind = PPO.load("ppo_lunar_primacy_calm_wind", env=env_wind)
+model_wind_calm = PPO.load("ppo_lunar_primacy_wind_calm", env=env_wind)
 
 # Evaluate the agent
-mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
+mean_reward_calm_wind, std_reward_calm_wind = evaluate_policy(model_calm_wind,
+                                          model_calm_wind.get_env(),
+                                          n_eval_episodes=10)
+mean_reward_wind_calm, std_reward_wind_calm = evaluate_policy(model_wind_calm,
+                                          model_wind_calm.get_env(),
+                                          n_eval_episodes=10)
 
-print(f"mean reward: {mean_reward}")
-print(f"std dev reward: {std_reward}")
+print(f"mean reward calm_wind: {mean_reward_calm_wind}")
+print(f"std dev reward calm_wind: {std_reward_calm_wind}")
+print(f"mean reward wind_calm: {mean_reward_wind_calm}")
+print(f"std dev reward wind_calm: {std_reward_wind_calm}")
 
 # Enjoy trained agent
-vec_env = model.get_env()
+print("Demo calm_wind")
+vec_env = model_calm_wind.get_env()
 obs = vec_env.reset()
 for i in range(1000):
-    action, _states = model.predict(obs, deterministic=True)
+    action, _states = model_calm_wind.predict(obs, deterministic=True)
+    obs, rewards, dones, info = vec_env.step(action)
+    vec_env.render()
+time.sleep(2)
+print("Demo wind_calm")
+vec_env = model_wind_calm.get_env()
+obs = vec_env.reset()
+for i in range(1000):
+    action, _states = model_wind_calm.predict(obs, deterministic=True)
     obs, rewards, dones, info = vec_env.step(action)
     vec_env.render()
