e374bd9da2a71bebfea7559260c12c3ec37bc583
Dante-Basile <dbasile1070@gmail.com>
2023-04-19 21:19:32 -0400

    good demos

--- lunar_lander_sbl_ppo_primacy.py
+++ lunar_lander_sbl_ppo_primacy.py
@@ -9,6 +9,8 @@ import numpy as np
 
 
 train = False
+test = False
+animate = True
 ver = "2"
 
 
@@ -40,64 +42,74 @@ if train:
     del model_calm_wind  # delete trained model to demonstrate loading
     del model_wind_calm
 
-# Load the trained agent
-model_calm_wind = PPO.load("ppo_lunar_primacy_calm_wind" + "_" + ver, env=env_wind)
-model_wind_calm = PPO.load("ppo_lunar_primacy_wind_calm" + "_" + ver, env=env_wind)
+if test:
 
-# Evaluate the agent
-mean_reward_calm_wind, std_reward_calm_wind = evaluate_policy(model_calm_wind,
-                                          model_calm_wind.get_env(),
-                                          n_eval_episodes=100)
-print(f"mean reward calm_wind: {mean_reward_calm_wind}")
-print(f"std dev reward calm_wind: {std_reward_calm_wind}")
+    # Load the trained agent
+    model_calm_wind = PPO.load("ppo_lunar_primacy_calm_wind" + "_" + ver, env=env_wind)
+    model_wind_calm = PPO.load("ppo_lunar_primacy_wind_calm" + "_" + ver, env=env_wind)
 
-mean_reward_wind_calm, std_reward_wind_calm = evaluate_policy(model_wind_calm,
-                                          model_wind_calm.get_env(),
-                                          n_eval_episodes=100)
+    # Evaluate the agent
+    mean_reward_calm_wind, std_reward_calm_wind = evaluate_policy(model_calm_wind,
+                                            model_calm_wind.get_env(),
+                                            n_eval_episodes=100)
+    print(f"mean reward calm_wind: {mean_reward_calm_wind}")
+    print(f"std dev reward calm_wind: {std_reward_calm_wind}")
 
-print(f"mean reward wind_calm: {mean_reward_wind_calm}")
-print(f"std dev reward wind_calm: {std_reward_wind_calm}")
+    mean_reward_wind_calm, std_reward_wind_calm = evaluate_policy(model_wind_calm,
+                                            model_wind_calm.get_env(),
+                                            n_eval_episodes=100)
 
-bar_x_calm_wind = [1]
-bar_x_wind_calm = [2]
-bar_y_calm_wind = [mean_reward_calm_wind]
-bar_y_wind_calm = [mean_reward_wind_calm]
-bar_err_calm_wind = [std_reward_calm_wind]
-bar_err_wind_calm = [std_reward_wind_calm]
-plt.bar(bar_x_calm_wind, bar_y_calm_wind, color='b')
-plt.bar(bar_x_wind_calm, bar_y_wind_calm, color='g')
-plt.errorbar(bar_x_calm_wind, bar_y_calm_wind, yerr=bar_err_calm_wind, fmt='o', color='r')
-plt.errorbar(bar_x_wind_calm, bar_y_wind_calm, yerr=bar_err_wind_calm, fmt='o', color='r')
-plt.legend(["calm->wind", "wind->calm"])
-plt.xticks([])
-plt.title("Agent Reward Comparison")
-plt.ylabel("reward")
-plt.savefig("ppo_lunar_primacy" + "_" + ver + ".png")
+    print(f"mean reward wind_calm: {mean_reward_wind_calm}")
+    print(f"std dev reward wind_calm: {std_reward_wind_calm}")
+    del model_calm_wind
+    del model_wind_calm
 
-# Enjoy trained agent
-env_disp = gym.make("LunarLander-v2", enable_wind=True, render_mode='rgb_array')
-print("Demo calm_wind")
-del model_calm_wind
-model_calm_wind = PPO.load("ppo_lunar_primacy_calm_wind" + "_" + ver, env=env_disp)
-vec_env = model_calm_wind.get_env()
-obs = vec_env.reset()
-frames_calm_wind = []
-for i in range(1000):
-    action, _states = model_calm_wind.predict(obs, deterministic=True)
-    obs, rewards, dones, info = vec_env.step(action)
-    frames_calm_wind.append(vec_env.render())
-imageio.mimsave("ppo_lunar_primacy_calm_wind" + "_" + ver + ".gif",
-                [np.array(img) for i, img in enumerate(frames_calm_wind) if i%2 == 0], fps=29)
+    bar_x_calm_wind = [1]
+    bar_x_wind_calm = [2]
+    bar_y_calm_wind = [mean_reward_calm_wind]
+    bar_y_wind_calm = [mean_reward_wind_calm]
+    bar_err_calm_wind = [std_reward_calm_wind]
+    bar_err_wind_calm = [std_reward_wind_calm]
+    plt.bar(bar_x_calm_wind, bar_y_calm_wind, color='b')
+    plt.bar(bar_x_wind_calm, bar_y_wind_calm, color='g')
+    plt.errorbar(bar_x_calm_wind, bar_y_calm_wind, yerr=bar_err_calm_wind, fmt='o', color='r')
+    plt.errorbar(bar_x_wind_calm, bar_y_wind_calm, yerr=bar_err_wind_calm, fmt='o', color='r')
+    plt.legend(["calm->wind", "wind->calm"])
+    plt.xticks([])
+    plt.title("Agent Reward Comparison")
+    plt.ylabel("reward")
+    plt.savefig("ppo_lunar_primacy" + "_" + ver + ".png")
 
-print("Demo wind_calm")
-del model_wind_calm
-model_wind_calm = PPO.load("ppo_lunar_primacy_wind_calm" + "_" + ver, env=env_disp)
-vec_env = model_wind_calm.get_env()
-obs = vec_env.reset()
-frames_wind_calm = []
-for i in range(1000):
-    action, _states = model_wind_calm.predict(obs, deterministic=True)
-    obs, rewards, dones, info = vec_env.step(action)
-    frames_wind_calm.append(vec_env.render())
-imageio.mimsave("ppo_lunar_primacy_wind_calm" + "_" + ver + ".gif",
-                [np.array(img) for i, img in enumerate(frames_wind_calm) if i%2 == 0], fps=29)
+if animate:
+
+    model_calm_wind = PPO.load("ppo_lunar_primacy_calm_wind" + "_" + ver, env=env_wind)
+    model_wind_calm = PPO.load("ppo_lunar_primacy_wind_calm" + "_" + ver, env=env_wind)
+
+    # Enjoy trained agent
+    env_disp = gym.make("LunarLander-v2", enable_wind=True, render_mode='rgb_array')
+    print("Demo calm_wind")
+    model_calm_wind = PPO.load("ppo_lunar_primacy_calm_wind" + "_" + ver, env=env_disp)
+    vec_env = model_calm_wind.get_env()
+    obs = vec_env.reset()
+    frames_calm_wind = []
+    for i in range(1000):
+        action, _states = model_calm_wind.predict(obs, deterministic=True)
+        obs, rewards, dones, info = vec_env.step(action)
+        frames_calm_wind.append(vec_env.render())
+    imageio.mimsave("ppo_lunar_primacy_calm_wind" + "_" + ver + ".gif",
+                    [np.array(img) for i, img in enumerate(frames_calm_wind) if i%2 == 0], fps=29)
+
+    print("Demo wind_calm")
+    model_wind_calm = PPO.load("ppo_lunar_primacy_wind_calm" + "_" + ver, env=env_disp)
+    vec_env = model_wind_calm.get_env()
+    obs = vec_env.reset()
+    frames_wind_calm = []
+    for i in range(1000):
+        action, _states = model_wind_calm.predict(obs, deterministic=True)
+        obs, rewards, dones, info = vec_env.step(action)
+        frames_wind_calm.append(vec_env.render())
+    imageio.mimsave("ppo_lunar_primacy_wind_calm" + "_" + ver + ".gif",
+                    [np.array(img) for i, img in enumerate(frames_wind_calm) if i%2 == 0], fps=29)
+
+    del model_calm_wind
+    del model_wind_calm
